30b9f060b0b58393965e84f330da4e33c881386a
illumio <s31388@pjwstk.edu.pl>
2025-12-28 15:43:44 +0100

    fix: add createdAt to recipe model

---
+++ backend/RecipeBlog/RecipeBlog.API/Migrations/20251228144221_RecipePatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBlog.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecipePatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Recipes",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Recipes");
+        }
+    }
+}
--- backend/RecipeBlog/RecipeBlog.API/Models/Recipe.cs
+++ backend/RecipeBlog/RecipeBlog.API/Models/Recipe.cs
@@ -6,6 +6,7 @@ public class Recipe
     public string Title { get; set; }
     public string Description { get; set; }
     public string ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
     
     public ICollection<Product> Products { get; set; } = new List<Product>();
     public int UserId { get; set; }
