7727273f4d942ebc2f5804397e9b505a9b5b7492
illumio <s31388@pjwstk.edu.pl>
2025-12-31 14:39:15 +0100

    fix: migration with deleted img added

--- backend/RecipeBlog/RecipeBlog.API/Migrations/20251231133458_DeleteImage.Designer.cs
+++ backend/RecipeBlog/RecipeBlog.API/Migrations/20251231133458_DeleteImage.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecipeBlog.API.DAL;
 
@@ -10,9 +11,11 @@ using RecipeBlog.API.DAL;
 namespace RecipeBlog.API.Migrations
 {
     [DbContext(typeof(RecipeBlogDbContext))]
-    partial class RecipeBlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251231133458_DeleteImage")]
+    partial class DeleteImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
