732ea09a90eca3aa778358d8a7e7d259862361fa
illumio <s31388@pjwstk.edu.pl>
2026-01-06 15:00:52 +0100

    feat: readme and validation

--- backend/RecipeBlog/RecipeBlog.API/DTO/CreateUserDTO.cs
+++ backend/RecipeBlog/RecipeBlog.API/DTO/CreateUserDTO.cs
@@ -1,3 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RecipeBlog.API.DTO;
 
-public record CreateUserDTO(string FullName, string UserEmail, string UserPassword);
+public record CreateUserDTO(
+    [Required(ErrorMessage = "Provide a name to proceed"), 
+     MinLength(1, ErrorMessage = "Name can not be empty")]
+    string FullName,
+    [Required(ErrorMessage = "Provide an email to proceed"),
+    EmailAddress(ErrorMessage = "Provide a valid email address")]
+    string UserEmail,
+    [Required(ErrorMessage = "Provide a password to proceed"),
+     MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+    string UserPassword);
